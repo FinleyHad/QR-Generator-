ea9dd67e4986bc9db5e142dcce1654baf9482038
Finley Hadfield <ff003767@student.reading.ac.uk>
2025-12-27 15:38:34 -0500

    Revert pad byte order to correct spec [0xEC, 0x11]

--- QRProject/qr/encoding/byte_mode.py
+++ QRProject/qr/encoding/byte_mode.py
@@ -1,5 +1,5 @@
 BYTE_MODE_INDICATOR = "0100"
-PAD_BYTES = [0x11, 0xEC]  # Alternating pad bytes (start with 0x11, not 0xEC)
+PAD_BYTES = [0xEC, 0x11]  # QR spec: cycle 0xEC, 0x11, 0xEC, 0x11, ...
 DATA_CODEWORDS = 19
 VERSION_1_L_DATA_BITS = DATA_CODEWORDS * 8
 
